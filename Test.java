26cac2a10ef419169a87d63e5613653d251e7f9f
AM021711 <z0919888300@gmail.com>
2015-04-15 11:33:08 +0800

    AM021711 李昱緯

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test
 {
 	public static void main(String[] args){
-		System.out.println("組長:李啟榮		AM021968")
+		System.out.println("組長:李啟榮		AM021968");
+
+		System.out.println("組員:李昱緯		AM021711");
 	}
 }
